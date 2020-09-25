52d2c0403c1749bfbd1e9b0f8158702f3cb2c169
Berantzino <lasse@berantzino.dk>
2020-09-25 03:33:13 +0200

    refactors to the use of Employee class instead of anonymous object

--- index.js
+++ index.js
@@ -5,9 +5,18 @@ console.log("Lab 03 -  NodeJs");
 //Define Server Port
 const port = process.env.PORT || 8081
 
+// Employee data
+employees = [
+    new employee.Employee(1, "Pritesh", "Patel", "pritesh@gmail.com", 5000),
+    new employee.Employee(2, "Krish", "Lee", "krish@gmail.com", 4000),
+    new employee.Employee(3, "Racks", "Jacson", "racks@gmail.com", 5500),
+    new employee.Employee(4, "Denial", "Roast", "denial@gmail.com", 9000)
+]
+
 //Create Web Server using CORE API
 const server = http.createServer((req, res) => {
     if (req.method !== 'GET') {
+        res.writeHead(405)
         res.end(`{"error": "${http.STATUS_CODES[405]}"}`)
     } else {
         if (req.url === '/') {
@@ -18,29 +27,26 @@ const server = http.createServer((req, res) => {
             // Sets statusCode to 200 OK and adds content type of json
             res.writeHead(200, {"Content-Type": "application/json"})
             //converts object to json
-            res.write(JSON.stringify(employee))
+            res.write(JSON.stringify(employees))
         }
 
         if (req.url === '/employee/names') {
             res.writeHead(200, {"Content-Type": "application/json"})
             res.write(JSON.stringify(
-                employee.employees.map((e, index) =>
-                    `${e.firstName} ${e.lastName}`).sort()
+                employees.map((e) => e.fullName).sort()
             ))
         }
 
         if (req.url === '/employee/totalsalary') {
-            //TODO - Display Sum of all employees salary in given JSON format 
-            //e.g. { "total_salary" : 100 }
-            let total_salary = employee.employees
-                .map((e) => e.Salary)
-                .reduce((a, b) => a + b, 0)
+            let total_salary = 
+                employees.map((e) => e.salary).reduce((a, b) => a + b, 0)
 
             res.writeHead(200, {"Content-Type": "application/json"})
             res.write(JSON.stringify({'total_salary': total_salary}))
         }
         res.end()
     }
+    res.writeHead(404)
     res.end(`{"error": "${http.STATUS_CODES[404]}"}`)
 })
 
